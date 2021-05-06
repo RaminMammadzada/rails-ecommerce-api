3a1c5821b8faca071107483ef0a7b8258838e58c
Ramin Mammadzada <remki-design@hotmail.com>
2021-05-06 06:00:45 +0300

    Add tests and factories for HTTP requests to products.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,9 @@
 # spec/factories/products.rb
 FactoryBot.define do
   factory :product do
-    name { Faker::Lorem.name }
-    price { Faker::Lorem.price }
-    details { Faker::Lorem.text }
+    name { Faker::Lorem.word }
+    price { Faker::Number.decimal(l_digits: 2) }
+    details { Faker::Lorem.sentence(word_count: 5) }
     category_id nil
   end
 end
