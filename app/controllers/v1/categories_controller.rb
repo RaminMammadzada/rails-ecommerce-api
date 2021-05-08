c1db63518ba8a0f936e65b63a8517a41db765d8b
Ramin Mammadzada <remki-design@hotmail.com>
2021-05-08 17:44:21 +0300

    Add version class, update routes so it will redirect the request to the specific controller depending on the version.

---
+++ app/controllers/v1/categories_controller.rb
@@ -0,0 +1,46 @@
+module V1
+  class CategoriesController < ApplicationController
+    before_action :set_category, only: %i[show update destroy]
+
+    # GET /categories
+    def index
+      @categories = current_user.categories
+      json_response(@categories)
+    end
+
+    # POST /categories
+    def create
+      # create categories belonging to current user
+      @category = current_user.categories.create!(category_params)
+      json_response(@category, :created)
+    end
+
+    # GET /categories/:id
+    def show
+      json_response(@category)
+    end
+
+    # PUT /categories/:id
+    def update
+      @category.update(category_params)
+      head :no_content
+    end
+
+    # DELETE /categories/:id
+    def destroy
+      @category.destroy
+      head :no_content
+    end
+
+    private
+
+    def category_params
+      # whitelist params
+      params.permit(:title)
+    end
+
+    def set_category
+      @category = Category.find(params[:id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories do
-    resources :products
+  
+  
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :categories do
+      resources :products
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
